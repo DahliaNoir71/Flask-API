843deceb2ef34b5d5043889999a57d441b838f4e
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-18 14:22:07 +0200

    Add docstrings to `register_routes` and `home` functions
    This commit adds docstrings to the `register_routes` and `home` functions in `services/routes.py`. These docstrings provide a clearer understanding of the input parameters and expected return values.

--- services/routes.py
+++ services/routes.py
@@ -2,7 +2,16 @@ from flask import render_template
 
 
 def register_routes(app):
-
+    """
+    :param app: The Flask application instance to which routes are being registered.
+    :return: None
+    """
     @app.route('/', methods=['GET'])
     def home():
+        """
+        Registers the routes for the given Flask application `app`.
+
+        :param app: The Flask application instance.
+        :return: None
+        """
         return render_template('home.html')
