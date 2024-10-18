a4492bbbf53499eb6859613f9fe203e650687962
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-18 12:47:38 +0200

    Add endpoint to fetch all employees
    Introduced a new API route to retrieve all employees. The implementation uses Flask's `jsonify` to return the employees data, and the route is dynamically generated using a base API URL and versioning.

--- services/api_routes.py
+++ services/api_routes.py
@@ -1,3 +1,11 @@
+from flask import jsonify
+from services.employees import employees
+
+VERSION = 1
+BASE_API_URL = f'/api/v{VERSION}/resources/'
+
 def register_api_routes(app):
 
-    pass
+    @app.route(BASE_API_URL + 'employees/all', methods=['GET'])
+    def api_employees_all():
+        return jsonify(employees)
