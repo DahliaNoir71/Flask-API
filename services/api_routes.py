3aca98c2aa1fc6f514d5efa0326ec89854cbea94
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-18 14:23:06 +0200

    Add endpoint for fetching employee by ID
    Introduced a new API endpoint to retrieve details of a specific employee using their unique identifier. Added documentation for both the new and existing routes to improve code clarity.

--- services/api_routes.py
+++ services/api_routes.py
@@ -5,7 +5,29 @@ VERSION = 1
 BASE_API_URL = f'/api/v{VERSION}/resources/'
 
 def register_api_routes(app):
-
+    """
+    :param app: The flask application instance where the API routes will be registered.
+    :return: None
+    """
     @app.route(BASE_API_URL + 'employees/all', methods=['GET'])
     def api_employees_all():
+        """
+        Register API routes for the provided Flask application.
+
+        :return: None
+        """
         return jsonify(employees)
+
+    @app.route(BASE_API_URL + 'employees/<int:id_employee>', methods=['GET'])
+    def api_employees_id(id_employee):
+        """
+        :param id_employee: The unique identifier for an employee.
+        :return: A JSON response containing the details of the employee with the given id.
+        """
+        result = []
+        for employee in employees:
+            if employee['id'] == id_employee:
+                result.append(employee)
+
+        return jsonify(result)
+
