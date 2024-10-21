3d20c5c3d53920810fefea43a82964aa3616796c
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-21 10:22:27 +0200

    Add user update and delete functionalities
    Implemented methods for updating and deleting users in the database. Removed the `create_users_table` function and its related SQL as it was unused. Updated docstrings for clarity and consistency.

--- services/db_users.py
+++ services/db_users.py
@@ -1,13 +1,6 @@
 from services.database import get_db_connection, DB_NAME
 
 USERS_REQUESTS = {
-    "create_users_table": """
-        CREATE TABLE IF NOT EXISTS Users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT,
-            email TEXT
-        )
-    """,
     "get_users": """
         SELECT 
             id, username, email
@@ -27,14 +20,23 @@ USERS_REQUESTS = {
             Users 
         WHERE 
             id = :user_id
+    """,
+    "update_user":
     """
+    UPDATE 
+        Users
+    SET 
+        username = :username, email = :email
+    WHERE 
+        id = :user_id
+    """,
+    "delete_user": "DELETE FROM Users WHERE id = :user_id"
 }
-
-def create_table_users(connection):
-    connection.execute(USERS_REQUESTS["create_users_table"])
 
 def get_users():
     """
+    Fetches the list of users from the database.
+
     :return: A list of user records from the database.
     """
     sql_connection = get_db_connection(DB_NAME)
@@ -45,8 +47,8 @@ def get_users():
 
 def get_user(user_id):
     """
-    :param user_id: The ID of the user to retrieve.
-    :return: The user information as a dictionary if found, otherwise None.
+    :param user_id: ID of the user to fetch from the database
+    :return: User record from the database
     """
     sql_connection = get_db_connection(DB_NAME)
     user = sql_connection.execute(USERS_REQUESTS["get_user"], {"user_id": user_id}).fetchone()
@@ -56,7 +58,7 @@ def get_user(user_id):
 
 def insert_user(username, email):
     """
-    :param username: The username of the user to be inserted into the database.
+    :param username: The name of the user to be inserted into the database.
     :param email: The email address of the user to be inserted into the database.
     :return: None
     """
@@ -69,13 +71,23 @@ def insert_user(username, email):
 
 def update_user(user_id, username, email):
     """
-    :param user_id: Unique identifier for the user to be updated
-    :param username: New username for the user
-    :param email: New email address for the user
+    :param user_id: The unique identifier for the user to be updated.
+    :param username: The new username to set for the user.
+    :param email: The new email to set for the user.
     :return: None
     """
     sql_connection = get_db_connection(DB_NAME)
     sql_connection.execute(USERS_REQUESTS["update_user"],
                            {"user_id": user_id, "username": username, "email": email})
+    sql_connection.commit()
+    sql_connection.close()
+
+def delete_user(user_id):
+    """
+    :param user_id: The ID of the user to delete from the database.
+    :return: None
+    """
+    sql_connection = get_db_connection(DB_NAME)
+    sql_connection.execute(USERS_REQUESTS["delete_user"], {"user_id": user_id})
     sql_connection.commit()
     sql_connection.close()
