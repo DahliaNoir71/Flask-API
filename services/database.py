2e1f445139ea616c704fa1834b3664ec6b73799c
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-21 08:53:20 +0200

    Add detailed docstrings to database service methods
    Enhanced the docstrings for `get_db_connection`, `create_tables`, `init_db`, `get_users`, and `insert_user` methods, providing more clarity on parameters, return types, and method functionalities. This improves maintainability and helps new developers understand the code quicker.

--- services/database.py
+++ services/database.py
@@ -26,8 +26,10 @@ SQL_REQUESTS = {
 
 def get_db_connection(db_name):
     """
-    :param db_name: Name of the database file to connect to
-    :return: A tuple containing the database connection and the cursor.
+    :param db_name: The name of the SQLite database to connect to.
+    :type db_name: str
+    :return: A connection object to the SQLite database with `row_factory` set to `sqlite3.Row` for accessing rows as dictionaries.
+    :rtype: sqlite3.Connection
     """
     connection = sqlite3.connect(db_name)
     connection.row_factory = sqlite3.Row
@@ -35,22 +37,35 @@ def get_db_connection(db_name):
 
 def create_tables(connection):
     """
-    :param cursor: The cursor used to interact with the database.
+    :param connection: Database connection object used to execute SQL commands
     :return: None
     """
     connection.execute(SQL_REQUESTS["create_users_table"])
 
 def init_db():
+    """
+    Initializes the database by establishing a connection and creating necessary tables.
+
+    :return: None
+    """
     sql_connection = get_db_connection(DB_NAME)
     create_tables(sql_connection)
 
 def get_users():
+    """
+    :return: A list of user records from the database.
+    """
     sql_connection = get_db_connection(DB_NAME)
     users = sql_connection.execute(SQL_REQUESTS["get_users"]).fetchall()
     sql_connection.close()
     return users
 
 def insert_user(username, email):
+    """
+    :param username: The username of the user to be inserted into the database.
+    :param email: The email address of the user to be inserted into the database.
+    :return: None
+    """
     sql_connection = get_db_connection(DB_NAME)
     sql_connection.execute(SQL_REQUESTS["insert_user"], {"username": username, "email": email})
     sql_connection.commit()
