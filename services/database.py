78cf916b508a94f4f644a65df2c5a496284c9da5
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-21 09:23:00 +0200

    Refactor database services for better modularization
    Moved SQL table creation logic to `services/db_users.py` for better separation of concerns. Removed redundant SQL queries and database interaction functions from `services/database.py` to streamline the database handling process.

--- services/database.py
+++ services/database.py
@@ -1,28 +1,7 @@
 import sqlite3
+from services.db_users import create_table_users
 
 DB_NAME = "users.db"
-
-SQL_REQUESTS = {
-    "create_users_table": """
-        CREATE TABLE IF NOT EXISTS Users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT,
-            email TEXT
-        )
-    """,
-    "get_users": """
-        SELECT 
-            id, username, email
-        FROM 
-            Users
-    """,
-    "insert_user": """
-        INSERT INTO Users 
-            (username, email)
-        VALUES
-            (:username, :email)
-    """
-}
 
 def get_db_connection(db_name):
     """
@@ -40,7 +19,7 @@ def create_tables(connection):
     :param connection: Database connection object used to execute SQL commands
     :return: None
     """
-    connection.execute(SQL_REQUESTS["create_users_table"])
+    create_table_users(connection)
 
 def init_db():
     """
@@ -51,25 +30,7 @@ def init_db():
     sql_connection = get_db_connection(DB_NAME)
     create_tables(sql_connection)
 
-def get_users():
-    """
-    :return: A list of user records from the database.
-    """
-    sql_connection = get_db_connection(DB_NAME)
-    users = sql_connection.execute(SQL_REQUESTS["get_users"]).fetchall()
-    sql_connection.close()
-    return users
 
-def insert_user(username, email):
-    """
-    :param username: The username of the user to be inserted into the database.
-    :param email: The email address of the user to be inserted into the database.
-    :return: None
-    """
-    sql_connection = get_db_connection(DB_NAME)
-    sql_connection.execute(SQL_REQUESTS["insert_user"], {"username": username, "email": email})
-    sql_connection.commit()
-    sql_connection.close()
 
 
 
