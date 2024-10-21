bc9bf620b3eac4420e9ed1f125ced4ec967f2694
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-21 12:04:07 +0200

    Enhance email validation and feedback in user API routes
    Added email uniqueness check to user addition and update routes, providing feedback via flash messages. Also, expanded HTTP methods for user addition to include GET and improved redirection on operations.

--- services/api_routes.py
+++ services/api_routes.py
@@ -1,5 +1,5 @@
-from flask import request, redirect, url_for
-from services.db_users import get_user, insert_user, update_user, delete_user
+from flask import request, redirect, url_for, flash
+from services.db_users import *
 
 VERSION = 1
 BASE_API_URL = f'/api/v{VERSION}/'
@@ -12,7 +12,7 @@ def register_api_routes(app):
     :return: None
     """
 
-    @app.route('/user/add', methods=['POST'])
+    @app.route('/user/add', methods=['GET', 'POST'])
     def api_add_user():
         """
         Registers API routes with the provided Flask application instance.
@@ -21,8 +21,14 @@ def register_api_routes(app):
         """
         username = request.form['username']
         email = request.form['email']
-        insert_user(username, email)
-        return redirect(url_for('users_list'))
+        if check_email_unicity(email):
+            insert_user(username, email)
+            return redirect(url_for('users_list'))
+        else:
+            flash(f"Email '{email}' is already in use. Please choose a different email.", 'error')
+            return redirect(url_for('add_user_form'))
+
+
 
     @app.route('/user/edit/<int:user_id>', methods=['POST'])
     def api_update_user(user_id):
@@ -37,8 +43,12 @@ def register_api_routes(app):
         if request.method == 'POST':
             username = request.form['username']
             email = request.form['email']
-            update_user(user_id, username, email)
-            return redirect(url_for('users_list'))
+            if check_email_unicity(email):
+                update_user(user_id, username, email)
+                return redirect(url_for('users_list'))
+            else:
+                flash(f"Email '{email}' is already in use. Please choose a different email.", 'error')
+                return redirect(url_for('edit_user_form', user_id = user_id))
 
     @app.route('/user/delete/<int:user_id>', methods=['POST'])
     def api_delete_user(user_id):
@@ -48,5 +58,6 @@ def register_api_routes(app):
 
         if request.method == 'POST':
             delete_user(user_id)
+            flash(f'Utilisateur avec ID {user_id} supprimé avec succès!', 'success')
             return redirect(url_for('users_list'))
 
